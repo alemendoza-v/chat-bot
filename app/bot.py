cb4aa61c7e4e6c821f7f597b69be90304914a051
Ale Mendoza <aletinmendoza@hotmail.com>
2022-11-02 21:32:14 -0600

    first test

--- app/bot.py
+++ app/bot.py
@@ -7,10 +7,29 @@ app = Flask(__name__)
 
 @app.route('/bot', methods=['POST'])
 def bot():
+    knownWords = {
+        "hello": "Hello, how are you?",
+        "good": "That's good!",
+        "bad": "That's bad!",
+        "bye": "Bye!",
+        "thanks": "You're welcome!",
+        "what can you do": "I'm a bot, I don't know much",
+        "what are you doing": "I'm here to help you",
+        "what are you doing?": "I'm here to help you",
+        "how are you?": "Good, and you?",
+        "how are you": "Good, and you?",
+    }
+
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
+    for word in incoming_msg.split():
+        if word in knownWords:
+            msg.body(knownWords[word])
+            responded = True
+            break
+    """
     if 'quote' in incoming_msg:
         # return a quote
         r = requests.get('https://api.quotable.io/random')
@@ -21,10 +40,12 @@ def bot():
             quote = 'I could not retrieve a quote at this time, sorry.'
         msg.body(quote)
         responded = True
+    """
     if 'cat' in incoming_msg:
         # return a cat pic
         msg.media('https://cataas.com/cat')
         responded = True
+    
     if not responded:
         msg.body('I only know about famous quotes and cats, sorry!')
     return str(resp)
